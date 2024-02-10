ca786b4af68764d35046d97fb800186c202229ff
Nikola Cvetanovic <nikola.cvetanovic7@gmail.com>
2024-02-10 16:24:40 +0100

    Update To Reflect New Changes

--- SellnBuy.Api/Program.cs
+++ SellnBuy.Api/Program.cs
@@ -1,79 +1,86 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using SellnBuy.Api.Repositories;
-using SellnBuy.Api.Repositories.InMemRepositories;
 using SellnBuy.Api.Entities;
-using SellnBuy.Api.Settings;
 using MongoDB.Driver;
 using Microsoft.AspNetCore.Diagnostics;
 using System.Net;
 using System.ComponentModel.DataAnnotations;
 using SellnBuy.Api.Exceptions;
 using SellnBuy.Api.Services;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson;
+using SellnBuy.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using SellnBuy.Api.Entities.DTOs;
+using SellnBuy.Api.Entities.Mapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers(options => 
+builder.Services.AddControllers(options =>
 {
 	options.SuppressAsyncSuffixInActionNames = false;
 });
+
+// For MongoDb
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<IRepository<User>, InMemUsersRepository>();
-builder.Services.AddSingleton<IRepository<Advertisement>, InMemAdvertisementsRepository>();
+// InMemRepositories
+// builder.Services.AddSingleton<IRepository<User>, InMemUserRepository>();
+// builder.Services.AddSingleton<IRepository<Advertisement>, InMemAdvertisementRepository>();
+
+builder.Services.AddAutoMapper(typeof(UserMappingProfile));
+builder.Services.AddAutoMapper(typeof(AdvertisementMappingProfile));
+
+var connectionString = builder.Configuration.GetConnectionString("SellnBuyContext");
+builder.Services.AddSqlServer<SellnBuyContext>(connectionString);
+
+builder.Services.AddScoped<IRepository<User>, EntityFrameworkUsersRepository>();
+builder.Services.AddScoped<IRepository<Advertisement>, EntityFrameworkAdvertisementsRepository>();
 
 builder.Services.AddScoped<IService<User, UserDto, CreateUserDto, UpdateUserDto>, UsersService>();
 builder.Services.AddScoped<IService<Advertisement, AdvertisementDto, CreateAdvertisementDto, UpdateAdvertisementDto>, AdvertisementsService>();
 
-//builder.Services.AddSingleton<IRepository<User>, >();
-//builder.Services.AddSingleton<IRepository<Advertisement>, InMemAdvertisementsRepository>();
-
-var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-
-builder.Services.AddSingleton<IMongoClient>(ServiceProvider => 
-{
-	return new MongoClient(mongoDbSettings?.ConnectionString);
-});
-
-
-
 var app = builder.Build();
+
+app.Services.InitializeDb();
 
 app.UseExceptionHandler(errorApp =>
 {
-    errorApp.Run(async context =>
-    {
-        var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
-        string errorMessage = exceptionHandlerPathFeature?.Error?.Message ?? "An unexpected error occurred.";
+	errorApp.Run(async context =>
+	{
+		var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+		string errorMessage = exceptionHandlerPathFeature?.Error?.Message ?? "An unexpected error occurred.";
 
-        switch (exceptionHandlerPathFeature?.Error)
-        {
-            case NotFoundException:
-                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-                errorMessage = "Not found";
-                break;
+		switch (exceptionHandlerPathFeature?.Error)
+		{
+			case NotFoundException:
+				context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+				errorMessage = "The item with requested ID does not exist.";
+				break;
 
-            case ValidationException:
-                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                break;
+			case ValidationException:
+				context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+				break;
 
-            case Exception:
-                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                break;
+			case Exception:
+				context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+				break;
 
-            default:
-                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                break;
-        }
+			default:
+				context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+				break;
+		}
 
-        await context.Response.WriteAsync(errorMessage);
-    });
+		await context.Response.WriteAsync(errorMessage);
+	});
 });
 
 
