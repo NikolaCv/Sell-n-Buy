5d6b30b8af8b5ed65e7dc9adb17aa2649a21a42e
Nikola Cvetanovic <nikola.cvetanovic7@gmail.com>
2024-02-11 19:53:35 +0100

    Add New Entities and Foreign Keys

--- SellnBuy.Api/Data/SellnBuyContext.cs
+++ SellnBuy.Api/Data/SellnBuyContext.cs
@@ -13,9 +13,35 @@ public class SellnBuyContext : DbContext
 
 	public DbSet<User> Users => Set<User>();
 	public DbSet<Advertisement> Advertisements => Set<Advertisement>();
+	public DbSet<Condition> Conditions => Set<Condition>();
+	public DbSet<Category> Categories => Set<Category>();
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
 		modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+		modelBuilder.Entity<Condition>()
+			.HasIndex(c => c.Name)
+			.IsUnique();
+
+		modelBuilder.Entity<Category>()
+			.HasIndex(c => c.Name)
+			.IsUnique();
+			
+		modelBuilder.Entity<Advertisement>()
+			.HasOne(a => a.User)
+			.WithMany()
+			.HasForeignKey(a => a.UserId);
+
+		modelBuilder.Entity<Advertisement>()
+			.HasOne(a => a.Condition)
+			.WithMany()
+			.HasForeignKey(a => a.ConditionId);
+
+   		 modelBuilder.Entity<Advertisement>()
+			.HasOne(a => a.Category)
+			.WithMany()
+			.HasForeignKey(a => a.CategoryId);
+
 	}
 }
