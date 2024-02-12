9834d2d05041d89956006e445ab84c5b3e714c6a
Nikola Cvetanovic <nikola.cvetanovic7@gmail.com>
2024-02-12 11:57:37 +0100

    Make searchTerm Optional

--- SellnBuy.Api/Services/IService.cs
+++ SellnBuy.Api/Services/IService.cs
@@ -10,7 +10,7 @@ namespace SellnBuy.Api.Services;
 public interface IService<T, TDto, CreateTDto, UpdateTDto>
 where T : BaseEntity
 {
-	Task<IEnumerable<TDto>> GetAllAsync(string? searchTerm);
+	Task<IEnumerable<TDto>> GetAllAsync(string? searchTerm = null);
 	Task<TDto> GetAsync(int id);
 	Task<(TDto, int)> CreateAsync(CreateTDto item);
 	Task DeleteAsync(int id);
