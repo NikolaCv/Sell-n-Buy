72c55cdf2e608f9fdfa266b44759aa002833a6c9
Nikola Cvetanovic <nikola.cvetanovic7@gmail.com>
2024-02-18 18:17:25 +0100

    Change UserId from Int to String

--- SellnBuy.Api/Entities/Advertisement.cs
+++ SellnBuy.Api/Entities/Advertisement.cs
@@ -8,7 +8,7 @@ public class Advertisement : BaseEntity
 	public string? Description { get; set; }
 	[Range(1, 10e8)] public decimal Price { get; set; }
 	public int ConditionId { get; set; }
-	public int UserId { get; set; }
+	public required string UserId { get; set; }
 	public int CategoryId { get; set; }
 	public Condition? Condition { get; set; }
 	public User? User { get; set; }
