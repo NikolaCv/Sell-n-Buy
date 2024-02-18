d8d593068ef9176a7c8f503a343dc596e3c35ff9
Nikola Cvetanovic <nikola.cvetanovic7@gmail.com>
2024-02-18 18:18:56 +0100

    Add StringIdRequiredAttribute

--- SellnBuy.Api/Entities/Validation/IdRequiredAttribute.cs
+++ SellnBuy.Api/Entities/Validation/IdRequiredAttribute.cs
@@ -2,10 +2,18 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SellnBuy.Api.Entities.Validation;
 
-public class IdRequiredAttribute : ValidationAttribute
+public class IntIdRequiredAttribute : ValidationAttribute
 {
 	public override bool IsValid(object? value)
 	{
 		return value != null && (int)value > 0;
+	}
+}
+
+public class StringIdRequiredAttribute : ValidationAttribute
+{
+	public override bool IsValid(object? value)
+	{
+		return value != null && (string)value != "";
 	}
 }
