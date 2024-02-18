75a946042f4593a09d02674c54abdeb07c6082d8
Nikola Cvetanovic <nikola.cvetanovic7@gmail.com>
2024-02-18 18:16:18 +0100

    Change Inheritance: DbContext to IdentityDbContext

--- SellnBuy.Api/Data/SellnBuyContext.cs
+++ SellnBuy.Api/Data/SellnBuyContext.cs
@@ -1,17 +1,18 @@
 using System.Reflection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SellnBuy.Api.Entities;
 
 namespace SellnBuy.Api.Data;
 
-public class SellnBuyContext : DbContext
+public class SellnBuyContext : IdentityDbContext<User, IdentityRole, string>
 {
 	public SellnBuyContext(DbContextOptions<SellnBuyContext> options)
 		: base(options)
 	{
 	}
 
-	public DbSet<User> Users => Set<User>();
 	public DbSet<Advertisement> Advertisements => Set<Advertisement>();
 	public DbSet<Condition> Conditions => Set<Condition>();
 	public DbSet<Category> Categories => Set<Category>();
@@ -20,25 +21,41 @@ public class SellnBuyContext : DbContext
 	{
 		modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
+		modelBuilder.Entity<IdentityRole>().HasData(
+			new IdentityRole { Name = "User", NormalizedName = "USER" },
+			new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" }
+		);
+
+		modelBuilder.Entity<IdentityUserLogin<string>>()
+			.HasKey(l => new { l.LoginProvider, l.ProviderKey });
+		modelBuilder.Entity<IdentityUserRole<string>>()
+ 			.HasKey(r => new { r.UserId, r.RoleId });
+		modelBuilder.Entity<IdentityUserToken<string>>()
+			.HasKey(t => new { t.UserId, t.LoginProvider, t.Name });
+
 		modelBuilder.Entity<Condition>()
 			.HasIndex(c => c.Name)
 			.IsUnique();
+		modelBuilder.Entity<Condition>()
+			.Property(c => c.Name)
+			.IsRequired();
 
 		modelBuilder.Entity<Category>()
 			.HasIndex(c => c.Name)
 			.IsUnique();
-			
+		modelBuilder.Entity<Category>()
+			.Property(c => c.Name)
+			.IsRequired();
+
 		modelBuilder.Entity<Advertisement>()
 			.HasOne(a => a.User)
 			.WithMany()
 			.HasForeignKey(a => a.UserId);
-
 		modelBuilder.Entity<Advertisement>()
 			.HasOne(a => a.Condition)
 			.WithMany()
 			.HasForeignKey(a => a.ConditionId);
-
-   		 modelBuilder.Entity<Advertisement>()
+		modelBuilder.Entity<Advertisement>()
 			.HasOne(a => a.Category)
 			.WithMany()
 			.HasForeignKey(a => a.CategoryId);
