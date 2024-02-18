f716c7a2373e8ebe171f4689778ddc4ed0f6e2cf
Nikola Cvetanovic <nikola.cvetanovic7@gmail.com>
2024-02-18 18:15:05 +0100

    Make User Inherit From IdentityUser

--- SellnBuy.Api/Entities/User.cs
+++ SellnBuy.Api/Entities/User.cs
@@ -1,10 +1,10 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 namespace SellnBuy.Api.Entities;
 
-public class User : BaseEntity
+public class User : IdentityUser
 {
 	[StringLength(50)] public required string Name { get; set; }
 	public string? Bio { get; set; }
-	public string? PhoneNumber { get; set; }
-	[StringLength(50)] public required string Email { get; set; }
+	public DateTimeOffset CreatedDate { get; set; }
 }
